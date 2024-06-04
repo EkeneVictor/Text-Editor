a5832d0c90419570422c5481134802562c0a0067
EkeneVictor <ekeneokwuchukwu35@gmail.com>
2024-06-04 10:28:46 +0100

    updates

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import *
-from tkinter import filedialog,messagebox
+from tkinter import filedialog, messagebox
 
 filename = None
 
@@ -32,8 +32,8 @@ def save_as():
     t = text.get(0.0, END)
     try:
         f.write(t.rstrip())
-    except:
-        messagebox.showerror(title='Oops!', message='Unable to save file')
+    except Exception as e:
+        messagebox.showerror(title='Oops!', message=f'Unable to save file: {e}')
 
 
 def open_file():
@@ -43,12 +43,47 @@ def open_file():
     text.insert(0.0, t)
 
 
+def undo_text():
+    try:
+        text.edit_undo()
+    except tk.TclError:
+        pass
+
+
+def redo_text():
+    try:
+        text.edit_redo()
+    except tk.TclError:
+        pass
+
+
+def cut_text(event=None):
+    text.event_generate("<<Cut>>")
+
+
+def copy_text(event=None):
+    text.event_generate("<<Copy>>")
+
+
+def paste_text(event=None):
+    text.event_generate("<<Paste>>")
+
+
+def delete_text(event=None):
+    if text.tag_ranges("sel"):
+        text.delete("sel.first", "sel.last")
+
+
+def select_all_text(event=None):
+    text.tag_add('sel', '1.0', 'end')
+
+
 root = Tk()
 root.title('Cipher Text Editor')
 root.minsize(width=500, height=500)
 root.maxsize(width=500, height=500)
 
-text = Text(root, width=500, height=500)
+text = Text(root, width=500, height=500, undo=True)  # Enable undo functionality
 text.pack()
 
 menubar = Menu(root)
@@ -58,8 +93,19 @@ filemenu.add_command(label='Open', command=open_file)
 filemenu.add_command(label='Save', command=save_file)
 filemenu.add_command(label='Save As', command=save_as)
 filemenu.add_separator()
-filemenu.add_command(label='uit', command=root.quit)
+filemenu.add_command(label='Exit', command=root.quit)
 menubar.add_cascade(label='File', menu=filemenu)
+
+editmenu = Menu(menubar)
+editmenu.add_command(label='Undo (Ctrl + Z)', command=undo_text)
+editmenu.add_command(label='Redo (Ctrl + Y)', command=redo_text)
+editmenu.add_separator()
+editmenu.add_command(label='Cut', command=cut_text)
+editmenu.add_command(label='Copy', command=copy_text)
+editmenu.add_command(label='Paste', command=paste_text)
+editmenu.add_command(label='Delete', command=delete_text)
+editmenu.add_command(label='Select All', command=select_all_text)
+menubar.add_cascade(label='Edit', menu=editmenu)
 
 root.config(menu=menubar)
 root.mainloop()
